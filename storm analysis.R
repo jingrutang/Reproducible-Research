e6f6e2e1e891ddfddc5a7a918b1da66715a4fe10
jingrutang <46234435+jingrutang@users.noreply.github.com>
2019-01-26 20:24:31 +0800

    Add files via upload

--- storm analysis.R
+++ storm analysis.R
@@ -1,3 +1,9 @@
+Health and Economic Impact of Weather Events in the US
+Storms and other severe weather events can cause both public health and economic problems for communities and municipalities. Many severe events can result in fatalities, injuries, and property damage, and preventing such outcomes to the extent possible is a key concern.
+This project involves exploring the U.S. National Oceanic and Atmospheric Administration's (NOAA) storm database. This database tracks characteristics of major storms and weather events in the United States, including when and where they occur, as well as estimates of any fatalities, injuries, and property damage.
+
+Synopsis
+The analysis on the storm event database revealed that tornadoes are the most dangerous weather event to the population health. The second most dangerous event type is the excessive heat. The economic impact of weather events was also analyzed. Flash floods and thunderstorm winds caused billions of dollars in property damages between 1950 and 2011. The largest crop damage caused by drought, followed by flood and hails.
 
 # data processing 
 storm <- read.csv(bzfile("data/repdata-data-StormData.csv.bz2"))
@@ -27,7 +33,7 @@ casualties <- ddply(storm, .(EVTYPE), summarize,
 
 fatal_events <- head(casualties[order(casualties$fatalities, decreasing = T), ], 10)
 injury_events <- head(casualties[order(casualties$injuries, decreasing = T), ], 10)
-Top 10 events that caused largest number of deaths are
+
 fatal_events[, c("EVTYPE", "fatalities")]
 ##             EVTYPE fatalities
 ## 737        tornado       5633

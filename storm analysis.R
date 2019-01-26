584c4d3e0415592f6670ad63ae02366d83300e01
jingrutang <46234435+jingrutang@users.noreply.github.com>
2019-01-26 20:25:14 +0800

    Update storm analysis.R

--- storm analysis.R
+++ storm analysis.R
@@ -1,8 +1,8 @@
-Health and Economic Impact of Weather Events in the US
+## Health and Economic Impact of Weather Events in the US
 Storms and other severe weather events can cause both public health and economic problems for communities and municipalities. Many severe events can result in fatalities, injuries, and property damage, and preventing such outcomes to the extent possible is a key concern.
 This project involves exploring the U.S. National Oceanic and Atmospheric Administration's (NOAA) storm database. This database tracks characteristics of major storms and weather events in the United States, including when and where they occur, as well as estimates of any fatalities, injuries, and property damage.
 
-Synopsis
+## Synopsis
 The analysis on the storm event database revealed that tornadoes are the most dangerous weather event to the population health. The second most dangerous event type is the excessive heat. The economic impact of weather events was also analyzed. Flash floods and thunderstorm winds caused billions of dollars in property damages between 1950 and 2011. The largest crop damage caused by drought, followed by flood and hails.
 
 # data processing 
